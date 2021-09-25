591fea595a58548827026fc7563733701cd88375
sngtmth <sngtmth@gmail.com>
2021-09-25 11:52:03 +0530

    added features of Employee Attendance Check

--- src/com/bridgeLabz/EmployeeWages.java
+++ src/com/bridgeLabz/EmployeeWages.java
@@ -5,6 +5,14 @@ public class EmployeeWages
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to EmployeeWage Computation Program on Master Branch");
+		
+		int IS_PRESENT_full_time=1;
+		double empcheck=Math.floor(Math.random()*10)%2;
+		if(empcheck==IS_PRESENT_full_time)
+			
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 	
 }
