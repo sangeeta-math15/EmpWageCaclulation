4a319181e31be9389f53fbc40ee0e716ca328789
sngtmth <sngtmth@gmail.com>
2021-09-25 12:09:45 +0530

    Calculate Employee Wage

--- src/com/bridgeLabz/EmployeeWages.java
+++ src/com/bridgeLabz/EmployeeWages.java
@@ -7,12 +7,16 @@ public class EmployeeWages
 		System.out.println("Welcome to EmployeeWage Computation Program on Master Branch");
 		
 		int IS_PRESENT_full_time=1;
+		int EMP_RATE_PER_HR=20;
+		int emphr=0,salary=0;
 		double empcheck=Math.floor(Math.random()*10)%2;
 		if(empcheck==IS_PRESENT_full_time)
-			
-			System.out.println("Employee is present");
+			emphr=8;
 		else
-			System.out.println("Employee is absent");
+			emphr=0;
+		salary=emphr*EMP_RATE_PER_HR;
+		System.out.println("EmpWage:" +salary);
 	}
+	
 	
 }
