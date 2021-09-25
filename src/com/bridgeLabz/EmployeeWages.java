b035334b096f5ef23dcfeaaa3608c32413e9b512
sngtmth <sngtmth@gmail.com>
2021-09-25 12:28:04 +0530

    added part time employee wage

--- src/com/bridgeLabz/EmployeeWages.java
+++ src/com/bridgeLabz/EmployeeWages.java
@@ -7,11 +7,14 @@ public class EmployeeWages
 		System.out.println("Welcome to EmployeeWage Computation Program on Master Branch");
 		
 		int IS_PRESENT_full_time=1;
+		int IS_PRESENT_part_time=2;
 		int EMP_RATE_PER_HR=20;
 		int emphr=0,salary=0;
-		double empcheck=Math.floor(Math.random()*10)%2;
+		double empcheck=Math.floor(Math.random()*10)%3;
 		if(empcheck==IS_PRESENT_full_time)
 			emphr=8;
+		else if(empcheck==IS_PRESENT_part_time)
+			emphr=4;
 		else
 			emphr=0;
 		salary=emphr*EMP_RATE_PER_HR;
