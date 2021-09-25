df41589da4b459eff0f2ecf0a9d42aec8be5b8bc
sngtmth <sngtmth@gmail.com>
2021-09-25 11:38:41 +0530

    welcome Message

---
+++ src/com/bridgeLabz/EmployeeWages.java
@@ -0,0 +1,10 @@
+package com.bridgeLabz;
+
+public class EmployeeWages 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to EmployeeWage Computation Program on Master Branch");
+	}
+	
+}
