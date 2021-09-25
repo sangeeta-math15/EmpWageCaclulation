7a3da5a98215c35636bb7e64e3b3eefe77e7c838
sngtmth <sngtmth@gmail.com>
2021-09-25 12:50:21 +0530

    calculate EmpWage using SwitchCase

--- src/com/bridgeLabz/EmployeeWages.java
+++ src/com/bridgeLabz/EmployeeWages.java
@@ -2,24 +2,28 @@ package com.bridgeLabz;
 
 public class EmployeeWages 
 {
+	public static final int IS_PRESENT_full_time=1;
+	public static final int IS_PRESENT_part_time=2;
+	public static final int EMP_RATE_PER_HR=20;
+	
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to EmployeeWage Computation Program on Master Branch");
 		
-		int IS_PRESENT_full_time=1;
-		int IS_PRESENT_part_time=2;
-		int EMP_RATE_PER_HR=20;
 		int emphr=0,salary=0;
-		double empcheck=Math.floor(Math.random()*10)%3;
-		if(empcheck==IS_PRESENT_full_time)
-			emphr=8;
-		else if(empcheck==IS_PRESENT_part_time)
-			emphr=4;
-		else
-			emphr=0;
+		int empcheck=(int)Math.floor(Math.random()*10)%3;
+		switch(empcheck)
+		{
+			case IS_PRESENT_full_time:
+				emphr=8;
+				break;
+			case IS_PRESENT_part_time:
+				emphr=4;
+				break;
+			default:
+				emphr=0;
+		}
 		salary=emphr*EMP_RATE_PER_HR;
 		System.out.println("EmpWage:" +salary);
-	}
-	
-	
+	}	
 }
